8bb90fea4055ab050fc0754c47dcbb4dd5c5dc65
sebnorth <sebnorth@gmail.com>
2020-12-19 19:29:32 +0100

    Ex12 + ChristmasTree

---
+++ ChristmasTree/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the tree height please: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int baseTree = 2 * n - 1;
+            int j;
+            
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                int a = 2 * i -1; // number of asterixes
+
+                // sequence of spaces 
+                for (j = 0; j < (baseTree - a)/2; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // sequence of asterixes
+                for (j = 0; j < a; j++)
+                {
+                    Console.Write("*");
+                }
+
+                // sequence of spaces 
+                for (j = 0; j < (baseTree - a) / 2; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise12/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a number please: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n-1; i >= 0; i--) // i - counting rows, also number of spaces
+            {
+                if (i == n-1 || i == n-2 || i == 0)
+                {
+                    // step 1. printing out spaces
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    // step 2. printing out 'X', x = n-s = n - i
+                    for (int k = 0; k < n - i; k++)
+                    {
+                        Console.Write("X");
+                    }
+
+                    // new line
+                    Console.WriteLine();
+                }
+                else
+                {
+
+                    // step 1. printing out spaces
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    // print out 1 star
+                    Console.Write("X");
+
+                    for (int k = 0; k < n - i - 2; k++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    // print out 1 star
+                    Console.Write("X");
+
+                    // new line
+                    Console.WriteLine();
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
