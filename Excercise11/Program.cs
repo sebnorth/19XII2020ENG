29eb0a8af9619c2a85aaa2525ffad61d3f39b18e
sebnorth <sebnorth@gmail.com>
2020-12-19 18:49:16 +0100

    Dodaj pliki projektów.

---
+++ Excercise11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a number please: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 4; i >= 0; i--) // i - counting rows, also number of spaces
+            {
+                // step 1. printing out spaces
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // step 2. printing out 'X', x = n-s = n - i
+                for (int k = 0; k < n-i; k++)
+                {
+                    Console.Write("X");
+                }
+
+                // new line
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // step 1 - create a triange with a specific character '*'
+            // step 2 - replace '*' by a number
+
+            Console.WriteLine("Give a number please: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) // i - row number
+            {
+                for (int j = 0; j <= i; j++) // j - number of columns
+                {
+                    Console.Write("{0,4} ", (i+1)*(j+1)); 
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
